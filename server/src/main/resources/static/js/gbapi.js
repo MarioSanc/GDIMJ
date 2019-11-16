9fa7d45fc93ed99a677095cc19a97bf74933e463
ivan-rodnov <57326596+ivan-rodnov@users.noreply.github.com>
2019-11-16 15:56:19 +0100

    erro del .then en login(gbapi)

--- server/src/main/resources/static/js/gbapi.js
+++ server/src/main/resources/static/js/gbapi.js
@@ -14,41 +14,41 @@
  * El estado global de la aplicación.
  */
 class GlobalState {
-  constructor(classes, students, users, messages) {
-    this.classes = classes || [];
-    this.students = students || [];
-    this.users = users || [];
-    this.messages = messages || [];
-  }
-}
-
-/**
-* Agrupa estudiantes y profesores
-*/
-class EClass {
-    constructor(cid, students, teachers) {
-    this.cid = cid;     // id de la clase
-    this.students = students || []; // students; por SIDs (dnis o similar de estudiente)
-    this.teachers = teachers || []; // profes; por UIDs (dnis o similar de "User")
-  }
-}
-
-/**
-* Un estudiante
-*/
-class Student {
-    constructor(sid, first_name, last_name, cid, guardians) {
-      this.sid = sid;
-      this.first_name = first_name;
-      this.last_name = last_name;
-      this.cid = cid;
-      this.guardians = guardians || []; // profes; por UIDs (dnis o similar de "User")
+    constructor(classes, students, users, messages) {
+        this.classes = classes || [];
+        this.students = students || [];
+        this.users = users || [];
+        this.messages = messages || [];
     }
 }
 
 /**
-* Tipos de usuario
-*/
+ * Agrupa estudiantes y profesores
+ */
+class EClass {
+    constructor(cid, students, teachers) {
+        this.cid = cid; // id de la clase
+        this.students = students || []; // students; por SIDs (dnis o similar de estudiente)
+        this.teachers = teachers || []; // profes; por UIDs (dnis o similar de "User")
+    }
+}
+
+/**
+ * Un estudiante
+ */
+class Student {
+    constructor(sid, first_name, last_name, cid, guardians) {
+        this.sid = sid;
+        this.first_name = first_name;
+        this.last_name = last_name;
+        this.cid = cid;
+        this.guardians = guardians || []; // profes; por UIDs (dnis o similar de "User")
+    }
+}
+
+/**
+ * Tipos de usuario
+ */
 const UserRoles = {
     ADMIN: 'admin',
     GUARDIAN: 'guardian',
@@ -66,15 +66,15 @@ class User {
         this.first_name = first_name;
         this.last_name = last_name;
         this.tels = tels || [];
-        this.classes = classes || [];   // cids de clases en las que tiene alumnos (solo si profe)
+        this.classes = classes || []; // cids de clases en las que tiene alumnos (solo si profe)
         this.students = students || []; // students; por SIDs (solo si padre)
         this.password = pass;
-    }    
+    }
 }
 
 /**
-* Tipos de mensaje
-*/
+ * Tipos de mensaje
+ */
 const MessageLabels = {
     SENT: 'sent',
     RECVD: 'received',
@@ -90,17 +90,17 @@ class Message {
     constructor(msgid, date, from, to, labels, title, body, parent) {
         this.msgid = msgid;
         this.date = date || new Date(),
-        this.from = from;
+            this.from = from;
         if (parent) {
             this.parent = parent; // msgid of parent, only if reply. If reply, no "to"
         } else {
-            this.to = to;               // array with UIDs and/or CIDs (to send to aclass)
+            this.to = to; // array with UIDs and/or CIDs (to send to aclass)
         }
         this.labels = labels || []; // of MessageLabels
         labels.forEach(label => Util.checkEnum(label, MessageLabels));
-        this.title = title;   
-        this.body = body;     
-    }    
+        this.title = title;
+        this.body = body;
+    }
 }
 
 
@@ -121,53 +121,53 @@ class Util {
             return;
         }
         if (valid.indexOf(a) === -1) {
-        throw Error(
-            "Invalid enum value " + a + 
-            ", expected one of " + valid.join(", "));
+            throw Error(
+                "Invalid enum value " + a +
+                ", expected one of " + valid.join(", "));
         }
     }
 
     /**
-    * Genera un entero aleatorio entre min y max, ambos inclusive
-    * @param {Number} min 
-    * @param {Number} max 
-    */
+     * Genera un entero aleatorio entre min y max, ambos inclusive
+     * @param {Number} min 
+     * @param {Number} max 
+     */
     static randomInRange(min, max) {
         return Math.floor(Math.random() * (max - min + 1)) + min
     }
 
     static randomChar(alphabet) {
-      return alphabet.charAt(Math.floor(Math.random() * alphabet.length));
+        return alphabet.charAt(Math.floor(Math.random() * alphabet.length));
     }
-    
+
     static randomString(count, alphabet) {
-      const n = count || 5;
-      const valid = alphabet || UPPER + LOWER + DIGITS;
-      return new Array(n).fill('').map(() => this.randomChar(valid)).join('');
+        const n = count || 5;
+        const valid = alphabet || UPPER + LOWER + DIGITS;
+        return new Array(n).fill('').map(() => this.randomChar(valid)).join('');
     }
 
     static randomPass() {
-      const n = 7;
-      const prefix = this.randomChar(UPPER) + this.randomChar(LOWER) + this.randomChar(DIGITS);
-      const valid = UPPER + LOWER + DIGITS;
-      return prefix + new Array(n-3).fill('').map(() => this.randomChar(valid)).join('');
+        const n = 7;
+        const prefix = this.randomChar(UPPER) + this.randomChar(LOWER) + this.randomChar(DIGITS);
+        const valid = UPPER + LOWER + DIGITS;
+        return prefix + new Array(n - 3).fill('').map(() => this.randomChar(valid)).join('');
     }
 
     /**
      * Genera un identificador "unico" de 5 caracteres
      */
     static randomWord(count, capitalized) {
-        return capitalized ? 
-             this.randomChar(UPPER) + this.randomString(count -1, LOWER) :
-             this.randomString(count, LOWER);
+        return capitalized ?
+            this.randomChar(UPPER) + this.randomString(count - 1, LOWER) :
+            this.randomString(count, LOWER);
     }
-    
+
     /**
      * Genera palabras al azar, de forma configurable
      */
-    static randomText(wordCount, allCapitalized, delimiter) {        
-        let words = [ this.randomWord(5, true)];
-        for (let i=1; i<(wordCount || 1); i++) words.push(this.randomWord(5, allCapitalized));
+    static randomText(wordCount, allCapitalized, delimiter) {
+        let words = [this.randomWord(5, true)];
+        for (let i = 1; i < (wordCount || 1); i++) words.push(this.randomWord(5, allCapitalized));
         return words.join(delimiter || ' ');
     }
 
@@ -193,7 +193,9 @@ class Util {
      * de https://stackoverflow.com/a/11935263/15472
      */
     static randomSample(array, size) {
-        var shuffled = array.slice(0), i = array.length, temp, index;
+        var shuffled = array.slice(0),
+            i = array.length,
+            temp, index;
         while (i--) {
             index = Math.floor((i + 1) * Math.random());
             temp = shuffled[index];
@@ -208,29 +210,29 @@ class Util {
      */
     static randomStudent(cid) {
         return new Student(
-            Util.randomString(), 
-            Util.randomText(1), Util.randomText(2, true), 
+            Util.randomString(),
+            Util.randomText(1), Util.randomText(2, true),
             cid, []);
     }
 
-   /**
+    /**
      * Genera un usuario del tipo indicado al azar, con una clase
      */
     static randomUser(type, classes, students) {
         let u = new User(
-            Util.randomString(), 
+            Util.randomString(),
             type,
-            Util.randomText(1), Util.randomText(2, true), 
+            Util.randomText(1), Util.randomText(2, true),
             Util.fill(Util.randomInRange(1, 3), () => {
-                return Util.fill(3, ()=>""+Util.randomInRange(100, 999))
+                return Util.fill(3, () => "" + Util.randomInRange(100, 999))
                     .join('-')
             }),
             classes,
             students,
             Util.randomPass()
         );
-        return u;        
-    }    
+        return u;
+    }
 
     /**
      * Llena un array con el resultado de llamar a una funcion
@@ -238,11 +240,11 @@ class Util {
     static fill(count, callback) {
         let f = callback;
         let results = [];
-        for (let i=0; i<count; i++) results.push(f());
+        for (let i = 0; i < count; i++) results.push(f());
         return results;
     }
 
-   /**
+    /**
      * Genera un mensaje
      */
     static randomMessage(users) {
@@ -250,11 +252,11 @@ class Util {
             Util.randomString(),
             Util.randomDate(new Date(), 10),
             Util.randomChoice(users),
-            Util.fill(Util.randomInRange(1,5), () => Util.randomChoice(users)),
-            Util.randomSample(Object.values(MessageLabels), Util.randomInRange(1,5)),
-            Util.randomText(Util.randomInRange(3,7)),        
-            Util.randomText(Util.randomInRange(10,20)));
-    }    
+            Util.fill(Util.randomInRange(1, 5), () => Util.randomChoice(users)),
+            Util.randomSample(Object.values(MessageLabels), Util.randomInRange(1, 5)),
+            Util.randomText(Util.randomInRange(3, 7)),
+            Util.randomText(Util.randomInRange(10, 20)));
+    }
 }
 
 
@@ -275,25 +277,25 @@ function getId(id, object) {
 
 // sube datos en json, espera json de vuelta; lanza error por fallos (status != 200)
 function go(url, method, data = {}) {
-  let params = {
-    method: method, // POST, GET, POST, PUT, DELETE, etc.
-    headers: {
-      "Content-Type": "application/json; charset=utf-8",
-    },
-    body: JSON.stringify(data)
-  };
-  if (method === "GET") {
-	  // GET requests cannot have body; I could URL-encode, but it would not be used here
-	  delete params.body;
-  }
-  console.log("sending", url, params)
-  return fetch(url, params).then(response => {
-    if (response.ok) {
-        return data = response.json();
-    } else {
-        response.text().then(t => {throw new Error(t + ", at " + url)});
+    let params = {
+        method: method, // POST, GET, POST, PUT, DELETE, etc.
+        headers: {
+            "Content-Type": "application/json; charset=utf-8",
+        },
+        body: JSON.stringify(data)
+    };
+    if (method === "GET") {
+        // GET requests cannot have body; I could URL-encode, but it would not be used here
+        delete params.body;
     }
-  })
+    console.log("sending", url, params)
+    return fetch(url, params).then(response => {
+        if (response.ok) {
+            return data = response.json();
+        } else {
+            response.text().then(t => { throw new Error(t + ", at " + url) });
+        }
+    })
 }
 
 // actualiza el estado de la aplicación con el resultado de una petición
@@ -312,7 +314,7 @@ function updateState(data) {
 }
 
 // el estado global
-let globalState = new GlobalState();   
+let globalState = new GlobalState();
 
 // la direccion del servidor
 let serverApiUrl = "//localhost:8080/api/";
@@ -333,8 +335,9 @@ function resolve(id) {
 
 // hace login. Todas las futuras operaciones usan el token devuelto
 function login(uid, pass) {
-    return go(serverApiUrl + "login", 'POST', {uid: uid, password: pass})
-        .then(d => { if (!d) return; serverToken = d.token; updateState(d);});
+    return go(serverApiUrl + "login", 'POST', { uid: uid, password: pass })
+        .then(d => { if (!d) return;
+            serverToken = d.token; return updateState(d); });
 }
 
 // hace logout, destruyendo el token usado
@@ -392,34 +395,34 @@ function initialize() {
 
 // cosas que estarán disponibles desde fuera de este módulo
 export {
-  
-  // Clases
-  EClass,        // a class; uses cid as id
-  Student,       // a student; uses sid as id
-  UserRoles,     // possible user roles
-  User,          // a user: admin, teacher or guardian; uses uid
-  MessageLabels, // possible message labels
-  Message,       // a message; uses msgid
-  GlobalState,   // the whole state of the application
-  
-  // Estado
-  globalState,   // el estado de la aplicación, según la última respuesta
-  resolve,       // consulta un id en la cache
-  connect,       // establece URL del servidor. Debe llamarse antes de nada
 
-  // Métodos. Todos (menos login / initialize) usan el token que devuelve login
-  login,       // (uid, pass) --> returns valid token for user
-  logout,      // ()          --> deletes a valid token
-  addClass,    // (eclass)
-  addStudent,  // (student)
-  addUser,     // (user)
-  rm,          // (cid || sid || uid || msgid) 
-  set,         // (eclass || student || user || message)
-  send,        // (message)
-  list,        // ()
+    // Clases
+    EClass, // a class; uses cid as id
+    Student, // a student; uses sid as id
+    UserRoles, // possible user roles
+    User, // a user: admin, teacher or guardian; uses uid
+    MessageLabels, // possible message labels
+    Message, // a message; uses msgid
+    GlobalState, // the whole state of the application
 
-  initialize,  // ()          --> se puede llamar sólo 1 vez, tras limpiar el servidor
+    // Estado
+    globalState, // el estado de la aplicación, según la última respuesta
+    resolve, // consulta un id en la cache
+    connect, // establece URL del servidor. Debe llamarse antes de nada
 
-  // Utilidades varias que no forman parte de la API
-  Util,
+    // Métodos. Todos (menos login / initialize) usan el token que devuelve login
+    login, // (uid, pass) --> returns valid token for user
+    logout, // ()          --> deletes a valid token
+    addClass, // (eclass)
+    addStudent, // (student)
+    addUser, // (user)
+    rm, // (cid || sid || uid || msgid) 
+    set, // (eclass || student || user || message)
+    send, // (message)
+    list, // ()
+
+    initialize, // ()          --> se puede llamar sólo 1 vez, tras limpiar el servidor
+
+    // Utilidades varias que no forman parte de la API
+    Util,
 };
