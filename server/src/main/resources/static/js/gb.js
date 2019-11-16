171a7179dcf0e7184a79714a21fce628b57b2faf
ivan-rodnov <57326596+ivan-rodnov@users.noreply.github.com>
2019-11-16 17:58:03 +0100

    administracion.html

--- server/src/main/resources/static/js/gb.js
+++ server/src/main/resources/static/js/gb.js
@@ -24,52 +24,52 @@ import * as Gb from './gbapi.js'
 //
 
 function createGroupItem(mensaje) {
-  const rid = 'x_' + Math.floor(Math.random()*1000000);
-  const hid = 'h_'+rid;
-  const cid = 'c_'+rid;
-  
-  const html = [
-    '<div class="card">',
-    '<div class="card-header" id="', hid, '">',
-    '  <h2 class="mb-0">',
-    '    <button class="btn btn-link" type="button"',
+    const rid = 'x_' + Math.floor(Math.random() * 1000000);
+    const hid = 'h_' + rid;
+    const cid = 'c_' + rid;
+
+    const html = [
+        '<div class="card">',
+        '<div class="card-header" id="', hid, '">',
+        '  <h2 class="mb-0">',
+        '    <button class="btn btn-link" type="button"',
         ' data-toggle="collapse" data-target="#', cid, '"',
-    '      aria-expanded="true" aria-controls="', rid, '">',
-    '<b class="msg mtitle">', mensaje.title, '</b>',
-    '<div class="msg mdate"> Enviado el ', 
-    new Intl.DateTimeFormat('es-ES').format(mensaje.date),
-    ' por ', mensaje.from,
-    '</div>',
-    '    </button>',
-    '  </h2>',
-    '</div>',
-    '',
-    '<div id="', cid, '" class="collapse show" aria-labelledby="', hid,'" ',
-       'data-parent="#accordionExample">',
-    '  <div class="card-body msg">',
-    mensaje.body,
-    '  </div>',
-    '</div>',
-    '</div>'
-  ];
-  return $(html.join(''));
+        '      aria-expanded="true" aria-controls="', rid, '">',
+        '<b class="msg mtitle">', mensaje.title, '</b>',
+        '<div class="msg mdate"> Enviado el ',
+        new Intl.DateTimeFormat('es-ES').format(mensaje.date),
+        ' por ', mensaje.from,
+        '</div>',
+        '    </button>',
+        '  </h2>',
+        '</div>',
+        '',
+        '<div id="', cid, '" class="collapse show" aria-labelledby="', hid, '" ',
+        'data-parent="#accordionExample">',
+        '  <div class="card-body msg">',
+        mensaje.body,
+        '  </div>',
+        '</div>',
+        '</div>'
+    ];
+    return $(html.join(''));
 }
 
 // funcion para generar datos de ejemplo: clases, mensajes entre usuarios, ...
 // se puede no-usar, o modificar libremente
 async function populate(classes, minStudents, maxStudents, minParents, maxParents, msgCount) {
-      const U = Gb.Util;
+    const U = Gb.Util;
 
-      // genera datos de ejemplo
-      let classIds = classes || ["1A", "1B", "2A", "2B", "3A", "3B"];
-      let minStudentsInClass = minStudents || 10;
-      let maxStudentsInClass = maxStudents || 20;
-      let minParentsPerStudent = minParents || 1;
-      let maxParentsPerStudent = maxParents || 3;
-      let userIds = [];
-      let tasks = [];
+    // genera datos de ejemplo
+    let classIds = classes || ["1A", "1B", "2A", "2B", "3A", "3B"];
+    let minStudentsInClass = minStudents || 10;
+    let maxStudentsInClass = maxStudents || 20;
+    let minParentsPerStudent = minParents || 1;
+    let maxParentsPerStudent = maxParents || 3;
+    let userIds = [];
+    let tasks = [];
 
-      classIds.forEach(cid => {
+    classIds.forEach(cid => {
         tasks.push(() => Gb.addClass(new Gb.EClass(cid)));
         let teacher = U.randomUser(Gb.UserRoles.TEACHER, [cid]);
         userIds.push(teacher.uid);
@@ -77,27 +77,27 @@ async function populate(classes, minStudents, maxStudents, minParents, maxParent
 
         let students = U.fill(U.randomInRange(minStudentsInClass, maxStudentsInClass), () => U.randomStudent(cid));
         students.forEach(s => {
-          tasks.push(() => Gb.addStudent(s));
-          let parents = U.fill(U.randomInRange(minParentsPerStudent, maxParentsPerStudent),
-            () => U.randomUser(Gb.UserRoles.GUARDIAN, [], [s.sid]));
-          parents.forEach( p => {
-            userIds.push(p.uid);
-            tasks.push(() =>  Gb.addUser(p));
-          });
+            tasks.push(() => Gb.addStudent(s));
+            let parents = U.fill(U.randomInRange(minParentsPerStudent, maxParentsPerStudent),
+                () => U.randomUser(Gb.UserRoles.GUARDIAN, [], [s.sid]));
+            parents.forEach(p => {
+                userIds.push(p.uid);
+                tasks.push(() => Gb.addUser(p));
+            });
         });
-      });
-      tasks.push(() => Gb.addUser(U.randomUser(Gb.UserRoles.ADMIN)));
-      U.fill(msgCount, () => U.randomMessage(userIds)).forEach(m => tasks.push(() => Gb.send(m)));
+    });
+    tasks.push(() => Gb.addUser(U.randomUser(Gb.UserRoles.ADMIN)));
+    U.fill(msgCount, () => U.randomMessage(userIds)).forEach(m => tasks.push(() => Gb.send(m)));
 
-      // los procesa en secuencia contra un servidor
-      for (let t of tasks) {
+    // los procesa en secuencia contra un servidor
+    for (let t of tasks) {
         try {
             console.log("Starting a task ...");
             await t().then(console.log("task finished!"));
         } catch (e) {
             console.log("ABORTED DUE TO ", e);
         }
-      }
+    }
 }
 
 //
@@ -105,46 +105,45 @@ async function populate(classes, minStudents, maxStudents, minParents, maxParent
 // Código de pegamento, ejecutado sólo una vez que la interfaz esté cargada.
 // Generalmente de la forma $("selector").cosaQueSucede(...)
 //
-$(function() { 
-  
-  // funcion de actualización de ejemplo. Llámala para refrescar interfaz
-  function update(result) {
-    try {
-      // vaciamos un contenedor
-      $("#accordionExample").empty();
-      // y lo volvemos a rellenar con su nuevo contenido
-      Gb.globalState.messages.forEach(m =>  $("#accordionExample").append(createGroupItem(m)));      
-      // y asi para cada cosa que pueda haber cambiado
-    } catch (e) {
-      console.log('Error actualizando', e);
+$(function() {
+
+    // funcion de actualización de ejemplo. Llámala para refrescar interfaz
+    function update(result) {
+        try {
+            // vaciamos un contenedor
+            $("#accordionExample").empty();
+            // y lo volvemos a rellenar con su nuevo contenido
+            Gb.globalState.messages.forEach(m => $("#accordionExample").append(createGroupItem(m)));
+            // y asi para cada cosa que pueda haber cambiado
+        } catch (e) {
+            console.log('Error actualizando', e);
+        }
     }
-  }
 
 
-  // Servidor a utilizar. También puedes lanzar tú el tuyo en local (instrucciones en Github)
-  Gb.connect("http://gin.fdi.ucm.es:8080/api/");
+    // Servidor a utilizar. También puedes lanzar tú el tuyo en local (instrucciones en Github)
+    Gb.connect("http://gin.fdi.ucm.es:8080/api/");
 
-  // ejemplo de login
-  Gb.login("16337065G", "ZX05e").then(d => console.log("login ok!", d));
+    // ejemplo de login
+    Gb.login("16337065G", "ZX05e").then(d => console.log("login ok!", d));
 
-  // ejemplo de crear una clase, una vez logeados
-  Gb.addClass({cid: "1A"})
+    // ejemplo de crear una clase, una vez logeados
+    Gb.addClass({ cid: "1A" })
 
-  // ejemplo de crear un usuario, una vez logueados como admin (los no-admin no pueden hacer eso)
-  Gb.addUser({
-	"uid": "18950946G",
-	"first_name": "Elena",
-	"last_name": "Enseña Enséñez",
-	"type": "teacher",
-	"tels": [ "141-456-781"],
-	"password" : "axarW!3",
-    "classes": [
-        "1A"
-    ]});
+    // ejemplo de crear un usuario, una vez logueados como admin (los no-admin no pueden hacer eso)
+    Gb.addUser({
+        "uid": "18950946G",
+        "first_name": "Elena",
+        "last_name": "Enseña Enséñez",
+        "type": "teacher",
+        "tels": ["141-456-781"],
+        "password": "axarW!3",
+        "classes": [
+            "1A"
+        ]
+    });
 });
 
 // cosas que exponemos para usarlas desde la consola
 window.populate = populate
 window.Gb = Gb;
-
-
